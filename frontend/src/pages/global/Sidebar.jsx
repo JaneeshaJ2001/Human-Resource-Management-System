fcb2848b36e5711503a0b7b656db7c5994bc75a3
JaneeshaJ2001 <wickramasinghejj.21@uom.lk>
2023-10-16 13:37:23 +0530

    Update Sidebar.jsx

--- frontend/src/pages/global/Sidebar.jsx
+++ frontend/src/pages/global/Sidebar.jsx
@@ -1,109 +1,102 @@
-/*
 import { useState } from "react";
-import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import { tokens } from "../../theme";
-import "react-pro-sidebar/dist/css/styles.css";
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import MoveItomOutlinedIcon from "@mui/icons-material/MoveItemOutlined";
-import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
-import DomainOutlinedIcon from "@mui/icons-material/DomainOutlined";
-import NoteOutlinedIcon from "@mui/icons-material/NoteOutlined";
-import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
-
-
+// import "react-pro-sidebar/dist/css/styles.css";
+// import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+// import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+// import MoveItomOutlinedIcon from "@mui/icons-material/MoveItemOutlined";
+// import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+// import DomainOutlinedIcon from "@mui/icons-material/DomainOutlined";
+// import NoteOutlinedIcon from "@mui/icons-material/NoteOutlined";
+// import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 
 const Sidebar = () => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-    const [isCollapsed, setIsCollapsed] = useState(false);
-    const [selected, setSelected] = useState("Dashboard");
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
 
-    return (
-        <Box
-          sx={{
-            "& .pro-sidebar-inner": {
-              background: `${colors.primary[400]} !important`,
-            },
-            "& .pro-icon-wrapper": {
-              backgroundColor: "transparent !important",
-            },
-            "& .pro-inner-item": {
-              padding: "5px 35px 5px 20px !important",
-            },
-            "& .pro-inner-item:hover": {
-              color: "#868dfb !important",
-            },
-            "& .pro-menu-item.active": {
-              color: "#6870fa !important",
-            },
-          }}
-        >
-          <ProSidebar collapsed={isCollapsed}>
-            <Menu iconShape="square">
-              
-              <MenuItem
-                onClick={() => setIsCollapsed(!isCollapsed)}
-                icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
-                style={{
-                  margin: "10px 0 20px 0",
-                  color: colors.grey[100],
-                }}
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+      }}
+    >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
               >
-                {!isCollapsed && (
-                  <Box
-                    display="flex"
-                    justifyContent="space-between"
-                    alignItems="center"
-                    ml="15px"
-                  >
-                    <Typography variant="h3" color={colors.grey[100]}>
-                      ADMINIS
-                    </Typography>
-                    <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
-                      <MenuOutlinedIcon />
-                    </IconButton>
-                  </Box>
-                )}
-              </MenuItem>
-    
-              {!isCollapsed && (
-                <Box mb="25px">
-                  <Box display="flex" justifyContent="center" alignItems="center">
-                    <img
-                      alt="profile-user"
-                      width="100px"
-                      height="100px"
-                      src={`../../assets/user.png`}
-                      style={{ cursor: "pointer", borderRadius: "50%" }}
-                    />
-                  </Box>
-                  <Box textAlign="center">
-                    <Typography
-                      variant="h2"
-                      color={colors.grey[100]}
-                      fontWeight="bold"
-                      sx={{ m: "10px 0 0 0" }}
-                    >
-                      Smith Jones
-                    </Typography>
-                    <Typography variant="h5" color={colors.greenAccent[500]}>
-                      HR Administrator
-                    </Typography>
-                  </Box>
-                </Box>
-              )}
-    
-              
-            </Menu>
-          </ProSidebar>
-        </Box>
-      );
-}
-*/
+                <Typography variant="h3" color={colors.grey[100]}>
+                  ADMINIS
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
 
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <img
+                  alt="profile-user"
+                  width="100px"
+                  height="100px"
+                  src={`../../assets/user.png`}
+                  style={{ cursor: "pointer", borderRadius: "50%" }}
+                />
+              </Box>
+              <Box textAlign="center">
+                <Typography
+                  variant="h2"
+                  color={colors.grey[100]}
+                  fontWeight="bold"
+                  sx={{ m: "10px 0 0 0" }}
+                >
+                  Smith Jones
+                </Typography>
+                <Typography variant="h5" color={colors.greenAccent[500]}>
+                  HR Administrator
+                </Typography>
+              </Box>
+            </Box>
+          )}
+        </Menu>
+      </ProSidebar>
+    </Box>
+  );
+};
+/*
 
 const Sidebar = () => {
     return (
@@ -112,5 +105,5 @@ const Sidebar = () => {
         </div>
     )
 }
-
+*/
 export default Sidebar;
