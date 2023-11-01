908d935db05cf6a42fab8bb287055b0cdaad9e5c
veejask-41 <sajeevk2001@gmail.com>
2023-11-01 14:30:47 +0530

    custom attributes

---
+++ server/routes/CustomAttributes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../database/connection");
+const dotenv = require("dotenv");
+dotenv.config();
+const { validateToken } = require("../middlewares/AuthMiddleware");
+
+router.get("/", validateToken, (req, res) => {
+  const query = "select * from custom_attributes";
+  db.query(query, (err, data) => {
+    if (err) {
+      res.json({ error: err });
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+module.exports = router;
